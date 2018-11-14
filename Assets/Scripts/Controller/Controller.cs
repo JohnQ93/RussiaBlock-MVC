38c082d22c6f225565af148cd95d06ef4988b491
Qiu Cheng <qiucheng93@163.com>
2018-11-14 17:15:32 +0800

    add UI content

---
+++ Assets/Scripts/Controller/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	[HideInInspector]
+	public Model model;
+	[HideInInspector]
+	public View view;
+
+	private void Awake()
+	{
+		model = GameObject.FindGameObjectWithTag("Model").GetComponent<Model>();
+		view = GameObject.FindGameObjectWithTag("View").GetComponent<View>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
