fc43ea4a8eea698b8e27385cc258fbb1c0f19b50
John_Q <qiucheng93@163.com>
2018-12-17 23:45:21 +0800

    delete rows

--- Assets/Scripts/Model/Model.cs
+++ Assets/Scripts/Model/Model.cs
@@ -35,5 +35,58 @@ public class Model : MonoBehaviour {
             Vector2 pos = child.position.Round();
             map[(int)pos.x, (int)pos.y] = child;
         }
+        CheckMap();
+    }
+
+    private void CheckMap()
+    {
+        for (int i = 0; i < MAX_ROWS; i++)
+        {
+            if (CheckRowFull(i))
+            {
+                DeleteRow(i);
+                MoveDownRowAbove(i + 1);
+                i--; //避免同时多行消除的时候只能消除一行
+            }
+        }
+    }
+
+    private bool CheckRowFull(int row)
+    {
+        for (int i = 0; i < MAX_COLUMNS; i++)
+        {
+            if (map[i, row] == null) return false;
+        }
+        return true;
+    }
+
+    private void DeleteRow(int row)
+    {
+        for (int i = 0; i < MAX_COLUMNS; i++)
+        {
+            Destroy(map[i, row].gameObject);
+            map[i, row] = null;
+        }
+    }
+
+    private void MoveDownRowAbove(int row)
+    {
+        for (int i = row; i < MAX_ROWS; i++)
+        {
+            MoveDownRow(i);
+        }
+    }
+
+    private void MoveDownRow(int row)
+    {
+        for (int i = 0; i < MAX_COLUMNS; i++)
+        {
+            if (map[i, row] != null)
+            {
+                map[i, row - 1] = map[i, row];
+                map[i, row] = null;
+                map[i, row - 1].position += new Vector3(0, -1, 0);
+            }
+        }
     }
 }
